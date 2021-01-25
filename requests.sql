4d5625b4ca328ca79999992c0f27d23366c4f724
Celine_lhp5 <celine.louvel.76@outlook.fr>
2021-01-25 11:34:21 +0100

    partie1done

--- requests.sql
+++ requests.sql
@@ -1,7 +1,24 @@
+-- PARTIE 1
 -- ex 1
 CREATE DATABASE languages;
 
 -- ex 2
-CREATE DATABASE webDevelopment;
+CREATE DATABASE webDevelopment CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- ex 3
+CREATE DATABASE IF NOT EXISTS frameworks CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- ex 4
+CREATE DATABASE IF NOT EXISTS languages CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- ex 5
+DROP DATABASE languages;
+
+-- ex 6
+DROP DATABASE IF EXISTS frameworks;
+
+-- ex 7
+DROP DATABASE IF EXISTS languages;
+
+-- PARTIE 2
+-- ex 1
